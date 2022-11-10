4d4be5c57bca938ec097cb49b39512f7168aa9c8
Tab-Y <tlyoumans@gmail.com>
2022-11-09 19:24:43 -0500

    npm modules

--- server.js
+++ server.js
@@ -1,16 +1,36 @@
+const path = require('path');
 const express = require('express');
-const routes = require('./routes');
-// import sequelize connection
+
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+
+const routes = require('./controllers');
+const sequelize = require('./config/connections');
+const helpers = require('./utils/helpers');
+const auth = require('./utils/auth')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+  secret: 'Super secret secret',
+  resave: false,
+  saveUninitialized: false,
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
-// sync sequelize models to the database, then turn on the server
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
