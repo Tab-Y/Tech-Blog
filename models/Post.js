45b5aaf6c88df52455cbc77f0d15acc6bcd0280e
Tab-Y <tlyoumans@gmail.com>
2022-11-09 23:04:16 -0500

    post model set

--- models/Post.js
+++ models/Post.js
@@ -1,7 +1,51 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {}
+class Post extends Model { }
 
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        comment: {
+            type: DataTypes.STRING,
+            references: {
+                model: 'comments'
+            }
+        },
+        date_created: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        date_updated: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            dialectTypes: DataTypes.NOW,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'users',
+                key: 'id',
+            },
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'posts',
+      }
+)
 
 module.exports = Post;
