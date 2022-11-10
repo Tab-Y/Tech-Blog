880b2a4f02af9b57a93d2f74ea36c9931c68ecbf
Tab-Y <tlyoumans@gmail.com>
2022-11-09 19:31:41 -0500

    base for modals

---
+++ models/Index.js
@@ -0,0 +1,10 @@
+const Comment = require("./Comment");
+const Post = require("./Post");
+const User = require("./User");
+
+// the specific assosiations go in this file
+
+
+
+
+module.exports = { Comment, Post, User }
