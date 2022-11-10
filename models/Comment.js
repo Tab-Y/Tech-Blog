45f5a3b8fb6a970cd7f935b23899d0742b582491
Tab-Y <tlyoumans@gmail.com>
2022-11-10 18:49:58 -0500

    comment model update

--- models/Comment.js
+++ models/Comment.js
@@ -28,6 +28,22 @@ Comment.init(
             allowNull: false,
             dialectTypes: DataTypes.NOW,
         },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'user',
+                key: 'id'
+            },
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: 'post',
+                key: 'id'
+            },
+        }
     },
     {
         sequelize,
