3d18cda54c445d03cdaf8555d3e16cf5d51c7f8c
Tab-Y <tlyoumans@gmail.com>
2022-11-10 22:10:07 -0500

    bsc changes

--- models/Index.js
+++ models/Index.js
@@ -3,22 +3,24 @@ const Post = require("./Post.js");
 const User = require("./User.js");
 
 // the specific assosiations go in this file
-User.hasMany(Post, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
+// User.hasMany(Post, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
+// User.hasMany(Comment, {
+//     foreignKey: 'user_id',
+//     onDelete: 'CASCADE'
+// });
 
 Post.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Comment.belongsTo(User, {
-    foreignKey: 'user_id'
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
 });
 
 Post.hasMany(Comment, {
@@ -26,9 +28,9 @@ Post.hasMany(Comment, {
     onDelete: 'CASCADE'
 });
 
-Comment.belongsTo(Post, {
-    foreignKey: 'post_id'
-});
+// Comment.belongsTo(Post, {
+//     foreignKey: 'post_id'
+// });
 
 
 module.exports = { Comment, Post, User }
