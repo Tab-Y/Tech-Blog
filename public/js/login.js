286b07658865c7422cf95bd1b436202e328cb945
Tab-Y <tlyoumans@gmail.com>
2022-11-11 14:32:14 -0500

    sign in capable

--- public/js/login.js
+++ public/js/login.js
@@ -9,13 +9,13 @@ const loginFormHandler = async (event) => {
         // Send a POST request to the API endpoint
         const response = await fetch('/api/users/login', {
             method: 'POST',
-            body: JSON.stringify({ email, password }),
+            body: JSON.stringify({ username, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
             // If successful, redirect the browser to the profile page
-            document.location.replace('/profile');
+            document.location.replace('/');
         } else {
             alert(response.statusText);
         }
@@ -24,4 +24,4 @@ const loginFormHandler = async (event) => {
 
 document
     .querySelector('.login-form')
-    .addEventListener('submit', loginFormHandler);
+    .addEventListener('click', loginFormHandler);
