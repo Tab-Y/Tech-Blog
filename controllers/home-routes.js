d8dabf16c0041a70141a2eab3f4a08af2890292d
Tab-Y <tlyoumans@gmail.com>
2022-11-21 14:56:38 -0500

    updating heroku

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,25 +4,21 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // // Get all projects and JOIN with user data
-    // const postData = await Post.findAll({
-    //   include: [
-    //     { model: User},
-    //   ],
-    // });
+    // Get all projects and JOIN with user data
+    const postData = await Post.findAll({
+      include: [
+        { model: User},
+      ],
+    });
 
-    // // Serialize data so the template can read it
-    // const posts = postData.map((post) => post.get({ plain: true }));
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
 
-    // // Pass serialized data and session flag into template
-    // res.render('all-posts', { 
-    //   posts, 
-    //   logged_in: req.session.logged_in 
-    // });
-
-    res.render('homepage',{
-      
-    })
+    // Pass serialized data and session flag into template
+    res.render('all-posts', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
   } catch (err) {
     res.status(500).json(err);
   }
